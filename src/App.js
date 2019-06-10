027af8b84779ea4015559af50adfaa17aca740c1
Jason Clouse <jhclouse@gmail.com>
2019-06-10 09:37:40 -0400

    Added tab bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import TrendingUpIcon from "@material-ui/icons/TrendingUp";
+import TableChartIcon from "@material-ui/icons/TableChart";
+
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+
+const TabContainer = props => (
+  <Typography component="div" style={{ padding: 8 * 3 }}>
+    {props.children}
+  </Typography>
+);
 
 function App() {
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => setValue(newValue);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static">
+        <Tabs value={value} onChange={handleChange}>
+          <Tab label="Data" icon={<TableChartIcon />} />
+          <Tab label="Graph" icon={<TrendingUpIcon />} />
+        </Tabs>
+      </AppBar>
+      {value === 0 && <TabContainer>Data Table</TabContainer>}
+      {value === 1 && <TabContainer>Graph</TabContainer>}
     </div>
   );
 }
