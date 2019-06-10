db3aecbb61a08f73177116cbbde75124ad228aed
Jason Clouse <jhclouse@gmail.com>
2019-06-10 09:52:35 -0400

    Added table component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
 
+// Data
+import widgets from "./WidgetData";
+
+// Icons
 import TrendingUpIcon from "@material-ui/icons/TrendingUp";
 import TableChartIcon from "@material-ui/icons/TableChart";
 
+// Tab bar
 import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
+
+// Data table
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableCell from "@material-ui/core/TableCell";
+import TableBody from "@material-ui/core/TableBody";
+import TableRow from "@material-ui/core/TableRow";
 
 const TabContainer = props => (
   <Typography component="div" style={{ padding: 8 * 3 }}>
@@ -26,7 +38,29 @@ function App() {
           <Tab label="Graph" icon={<TrendingUpIcon />} />
         </Tabs>
       </AppBar>
-      {value === 0 && <TabContainer>Data Table</TabContainer>}
+      {value === 0 && (
+        <TabContainer>
+          <Table>
+            <TableHead>
+              <TableCell>Name</TableCell>
+              <TableCell>Description</TableCell>
+              <TableCell>Revenue</TableCell>
+              <TableCell>Time</TableCell>
+            </TableHead>
+
+            <TableBody>
+              {widgets.map(widget => (
+                <TableRow>
+                  <TableCell>{widget.name}</TableCell>
+                  <TableCell>{widget.description}</TableCell>
+                  <TableCell>{widget.revenue}</TableCell>
+                  <TableCell>{widget.timestamp}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TabContainer>
+      )}
       {value === 1 && <TabContainer>Graph</TabContainer>}
     </div>
   );
